01784b283340e245b2dd2e4116c9512c6e1ee2e6
Dario Oddenino <branch13@gmail.com>
2018-10-15 17:04:28 +0200

    added some tests

--- test/Main.purs
+++ test/Main.purs
@@ -3,11 +3,20 @@ module Test.Main where
 import Prelude
 import Effect (Effect)
 import Test.Assert (assert)
-import Record.Prefix (addPrefix)
+import Record.Prefix (addPrefix, removePrefix)
 import Type.Prelude (SProxy(..))
 
+foo :: { bar :: Int, baz :: Boolean }
 foo = { bar: 1, baz: true }
+
+prefoo :: { prebar :: Int, prebaz :: Boolean }
+prefoo = { prebar: 1, prebaz: true }
+
+pre :: SProxy "pre"
+pre = SProxy
 
 main :: Effect Unit
 main = do
-  assert $ addPrefix (SProxy :: SProxy "pre") foo == { prebar: 1, prebaz: true }
+  assert $ addPrefix pre foo == prefoo
+  assert $ removePrefix pre prefoo == foo
+  assert $ foo == (removePrefix pre $ addPrefix pre foo)
