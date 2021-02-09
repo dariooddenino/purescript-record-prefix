e0df70e6bc2767e531dd07aa3bf09bd2a68dbc23
Tomasz Rybarczyk <paluho@gmail.com>
2021-02-09 14:10:18 +0100

    Precompute output row type when prefixing `Variant`

--- src/Data/Variant/Prefix.purs
+++ src/Data/Variant/Prefix.purs
@@ -5,48 +5,79 @@ import Data.Variant (Variant, inj)
 import Data.Variant (inj) as Variant
 import Heterogeneous.Folding (class FoldingWithIndex, class HFoldlWithIndex, hfoldlWithIndex)
 import Prim.Row (class Cons) as Row
+import Prim.RowList (Cons, Nil) as RowList
 import Prim.Symbol (class Append) as Symbol
-import Type.Prelude (class IsSymbol, SProxy(..))
+import Type.Eval (class Eval, kind TypeExpr)
+import Type.Eval.Foldable (FoldrWithIndex)
+import Type.Eval.Function (type (<<<))
+import Type.Eval.RowList (FromRow, ToRow)
+import Type.Prelude (class IsSymbol, RLProxy, RProxy, SProxy(..))
 
-data PrefixCases (s :: Symbol)
+foreign import data RowFoldBase :: TypeExpr
+
+instance evalRowFoldBase ∷ Eval RowFoldBase (RLProxy RowList.Nil)
+
+foreign import data PrefixStep :: Symbol -> Type -> Type -> TypeExpr -> TypeExpr
+
+instance evalPerfixStep ∷
+  ( Symbol.Append prefix label label'
+  , Eval tail (RLProxy tail')
+  , IsSymbol label'
+  ) ⇒
+  Eval (PrefixStep prefix (SProxy label) value tail) (RLProxy (RowList.Cons label' value tail'))
+
+data PrefixCases (s :: Symbol) (result ∷ # Type)
   = PrefixCases
 
 instance prefixCases ::
   ( Symbol.Append s l l'
   , IsSymbol l'
-  , Row.Cons l' a v v'
+  , Row.Cons l' a result_ result
   ) =>
-  FoldingWithIndex (PrefixCases s) (SProxy l) Unit a (Variant v') where
+  FoldingWithIndex (PrefixCases s result) (SProxy l) Unit a (Variant result) where
   foldingWithIndex _ prop _ a = Variant.inj (SProxy :: SProxy l') a
 
-data UnprefixCases (s :: Symbol)
+foreign import data UnprefixStep :: Symbol -> Type -> Type -> TypeExpr -> TypeExpr
+
+instance evalunprefixStep ∷
+  ( Symbol.Append prefix label' label
+  , Eval tail (RLProxy tail')
+  , IsSymbol label'
+  ) ⇒
+  Eval (UnprefixStep prefix (SProxy label) value tail) (RLProxy (RowList.Cons label' value tail'))
+
+data UnprefixCases (s :: Symbol) (result ∷ # Type)
   = UnprefixCases
 
 instance unprefixCases ::
   ( Symbol.Append s l' l
   , IsSymbol l'
-  , Row.Cons l' a v v'
+  , Row.Cons l' a result_ result
   ) =>
-  FoldingWithIndex (UnprefixCases s) (SProxy l) Unit a (Variant v') where
+  FoldingWithIndex (UnprefixCases s result) (SProxy l) Unit a (Variant result) where
   foldingWithIndex _ prop _ a = Variant.inj (SProxy :: SProxy l') a
 
 add ::
   forall rin rout pre.
   IsSymbol pre =>
-  HFoldlWithIndex (PrefixCases pre) Unit (Variant rin) (Variant rout) =>
+  Eval ((ToRow <<< FoldrWithIndex (UnprefixStep pre) RowFoldBase <<< FromRow) (RProxy rin)) (RProxy rout) ⇒
+  HFoldlWithIndex (PrefixCases pre rout) Unit (Variant rin) (Variant rout) =>
   SProxy pre ->
   Variant rin ->
   Variant rout
-add p = hfoldlWithIndex (PrefixCases :: PrefixCases pre) unit
+add p = hfoldlWithIndex (PrefixCases :: PrefixCases pre rout) unit
 
 remove ::
   forall pre rin rout.
-  HFoldlWithIndex (UnprefixCases pre) Unit (Variant rin) (Variant rout) =>
+  Eval ((ToRow <<< FoldrWithIndex (UnprefixStep pre) RowFoldBase <<< FromRow) (RProxy rin)) (RProxy rout) ⇒
+  HFoldlWithIndex (UnprefixCases pre rout) Unit (Variant rin) (Variant rout) =>
   SProxy pre ->
   Variant rin ->
   Variant rout
-remove p = hfoldlWithIndex (UnprefixCases :: UnprefixCases pre) unit
+remove p = hfoldlWithIndex (UnprefixCases :: UnprefixCases pre rout) unit
 
--- | We need an anotation of the intermediate result
--- test = remove (SProxy ∷ SProxy "b") (remove (SProxy ∷ SProxy "foo") (inj (SProxy ∷ SProxy "foobar") 8 ∷ Variant ( foobar ∷ Int )))
-test = remove (SProxy ∷ SProxy "b") (remove (SProxy ∷ SProxy "foo") (inj (SProxy ∷ SProxy "foobar") 8 ∷ Variant ( foobar ∷ Int )) ∷ (Variant ( bar ∷ Int )))
+-- | No annotation needed for intermediate result
+test = remove (SProxy :: SProxy "b") (remove (SProxy :: SProxy "foo") v)
+  where
+  v :: Variant ( foobar :: Int )
+  v = inj (SProxy :: SProxy "foobar") 8
