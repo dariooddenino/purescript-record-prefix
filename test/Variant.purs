7b96fdffb94f0e95b6640d965fa559e723ed85ff
Tomasz Rybarczyk <paluho@gmail.com>
2021-11-11 10:50:06 +0100

    Update for PureScript 0.14

---
+++ test/Variant.purs
@@ -0,0 +1,27 @@
+module Test.Variant where
+
+import Prelude
+
+import Data.Variant (Variant, inj)
+import Data.Variant.Prefix as Prefix
+import Effect (Effect)
+import Test.Assert (assert)
+import Type.Prelude (Proxy(..))
+
+foo :: Variant (bar :: Int, baz :: Boolean)
+foo = inj (Proxy ∷ Proxy "bar") 1
+
+prefoo :: Variant (prebar :: Int, prebaz :: Boolean)
+prefoo = inj (Proxy ∷ Proxy "prebar") 1
+
+pr ∷ Proxy "pr"
+pr = Proxy
+
+e ∷ Proxy "e"
+e = Proxy
+
+suite ∷ Effect Unit
+suite = do
+  assert $ Prefix.add pr (Prefix.add e foo) == prefoo
+  assert $ Prefix.remove e (Prefix.remove pr prefoo) == foo
+  assert $ foo == (Prefix.remove pr $ Prefix.add pr foo)
